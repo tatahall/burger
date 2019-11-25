a5f59846501cead2977a7853d8984f9072477bcd
Tahnee Hall <tnhall2012@hotmai.com>
2019-11-25 10:15:44 -0500

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('Hot Mess Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('Lots-a Meat Burger', false);
+INSERT INTO burgers (burger_name, devour) VALUES ('The Faux-ger', false);
