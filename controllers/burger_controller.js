f176add640a9a7bb6e2039b2fdbb8ff543843e61
Tahnee Hall <tnhall2012@hotmai.com>
2019-11-27 23:30:26 -0500

    updated handlebars and burgers.js files

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -17,11 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devour"
-  ], [
-    req.body.burger_name, req.body.devour
-  ], function(result) {
+  burger.create([req.body.name], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -32,9 +28,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   //console.log("condition", condition);
 
-  burger.update({
-    devour: req.body.devour
-  }, condition, function(result) {
+  burger.update(req.body, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -20,23 +20,21 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#bu").val().trim(),
-        devour: 0,
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
+
+      var name = $("[name=name]").val().trim();
+      
+      if (name !== ""){
+        var newBurger = {name: name};
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(function(){
           location.reload();
-        }
-      );
+        })
+      }else{
+        $("[name=name]").val("");
+      }
+     
     });
   
     $(".delete-burger").on("click", function(event) {
